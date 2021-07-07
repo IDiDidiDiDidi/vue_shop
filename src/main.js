608845d6de55bae976cae20048ad2adbde89fd14
di <1529086578@qq.com>
2021-07-07 11:44:35 +0800

    完成了登录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
