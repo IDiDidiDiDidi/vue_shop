e6c8082fb3d20131ab8982509ead92308aedd1b6
di <1529086578@qq.com>
2021-07-13 08:50:52 +0800

    主页布局 + 用户页面

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 
 // 导入全局样式表
@@ -15,7 +17,17 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home },
+  // 这是一个重定向模式，是要我们访问了home，就会访问到/welcome这个页面
+  { 
+    path: '/home', 
+    component: Home, 
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome},
+      { path: '/users', component: Users}
+    ] 
+  },
+
 ]
 
 const router = new VueRouter({
